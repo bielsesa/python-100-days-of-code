28925faf5619f855190086943f69c7cfed7ebaaa
Biel Serrano Sanchez <bielsesa.dirinformatica@gmail.com>
2022-12-20 09:02:57 +0100

    day 001

---
+++ 001/03.py
@@ -0,0 +1,6 @@
+#Write your code below this line 👇
+
+# You can use the input function inside of the print function.
+# You can also use it inside len so it counts the number of characters
+# of the input received.
+print("Your name has " + str(len(input("What is your name? "))) + " characters.")
